ff30b30fb8149df66535059b67ef875466dd492c
Aaditya Bhoota <aadityabhoota@gmail.com>
2020-03-18 14:36:22 -0700

    idk

--- blocks/burnPlat.gd
+++ blocks/burnPlat.gd
@@ -1,24 +1,21 @@
 extends KinematicBody2D
-var timer = 0
 var players = {} 
 export var velocity = Vector2(0,75)
 
 func _physics_process(delta):
 	position.x += velocity.x * delta
 	position.y += velocity.y * delta 
-	if timer > 0:
-		timer += 1
+
 	for b in players:
-		if timer % 60 == 0:
+		players[b] += 1
+		if players[b] % 60 == 0:
 			b.modulate = Color.red
-		if timer % 75 == 0:
+		if players[b] % 65 == 0:
 			b.modulate = Color.white
 
 func _on_Area2D_body_entered(body):
 	players[body] = 1
-	timer = 1
 
 func _on_Area2D_body_exited(body):
 	body.modulate = Color.white
-	timer = 0
 	players.erase(body)
