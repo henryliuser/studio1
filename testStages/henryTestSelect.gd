b04dadfab33480c2066223526a3055cdc674f32d
henryliu <henryliuser@gmail.com>
2020-05-08 10:14:45 -0700

    testing build, static weapon sprites, and outline to pickup working

--- testStages/henryTestSelect.gd
+++ testStages/henryTestSelect.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 func _on_boomer_pressed():
-	get_tree().change_scene("res://testStages/hennyTest1.tscn")
+	get_tree().change_scene("res://testStages/hennyTest1c.tscn")
 	
 func _on_random_pressed():
 	get_tree().change_scene("res://testStages/hennyTest2.tscn")
 
 func _on_random2_pressed():
 	get_tree().change_scene("res://testStages/hennyTest2a.tscn")
+
+func _on_boomer2_pressed():
+	get_tree().change_scene("res://testStages/hennyTest1a.tscn")
--- weapons/equippable.gd
+++ weapons/equippable.gd
@@ -9,18 +9,18 @@ func _ready():
 	self.connect("picked_up", self, "_on_picked_up")
 
 func _physics_process(_delta):  # needs to be physics process cuz order
-	if name == "boomerangSniper": $sprite.material = null
+	$sprite.material = null
 	if equipped: position = lerp(position,player.Weapons.posList[itemName], 0.3)
 
 func activate(p):  # is within range to be picked up by a player
 	if not equipped:
-		if name == "boomerangSniper": $sprite.material = load("res://assets/Shaders/Outline.tres")
+		$sprite.material = load("res://assets/Shaders/Outline.tres")
 		if Input.is_action_just_pressed("p" + str(p.localNum) + "_equip"):
 			player = p
 			equipped = true
 			pnum = p.localNum
 			var x = global_position
-			if name == "boomerangSniper": $sprite.material = null
+			$sprite.material = null
 			get_parent().remove_child(self)
 			p.Weapons.add_child(self)
 			global_position = x
