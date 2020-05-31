1f138e5b6869f9a29e3b2e7c363f4d3f1ce8baab
henryliu <henryliuser@gmail.com>
2020-05-31 01:50:03 -0700

    finally added patrolling platform support

--- blocks/defaultPlat.gd
+++ blocks/defaultPlat.gd
@@ -2,31 +2,28 @@ extends KinematicBody2D
 export var moving = false
 export var velocity = Vector2(0,75)
 export var is_wall = false
-export var patrol_to = Vector2()
-export var patrol_time = 0
-onready var tween = $Tween
+export var patrol_x = Vector2()
+export var patrol_y = Vector2()
+export var lerp_x = 0.03
+export var lerp_y = 0.03
+export var lee = 20
+
 onready var og_pos = global_position
+onready var x_bound = Vector2(og_pos.x + patrol_x.x, og_pos.x + patrol_x.y)
+onready var y_bound = Vector2(og_pos.y + patrol_y.x, og_pos.y + patrol_y.y)
+onready var x_target = x_bound.y
+onready var y_target = y_bound.y
 
-func _ready():
-	if patrol_time > 0:
-		tween.interpolate_property(self, "global_position", global_position, 
-		global_position + patrol_to, patrol_time, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-		tween.interpolate_property(self, "global_position", global_position + patrol_to,
-		global_position, patrol_time, Tween.TRANS_CUBIC, Tween.EASE_OUT, patrol_time)
-		tween.repeat = true
-		tween.start()
-#	var eXtents = Vector2(og_pos.x + patrol_x.x, og_pos.x + patrol_x.y)
-#	var eYtents = Vector2(og_pos.y + patrol_y.x, og_pos.y + patrol_y.y)
-#	if patrol_x_time != 0:
-#		tween.interpolate_property(self, "global_position:x", og_pos.x,
-#		eXtents.y, patrol_x_time, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-#		tween.interpolate_property(self, "global_position:x", eXtents.y,
-#		eXtents.x, patrol_x_time, Tween.TRANS_CUBIC, Tween.EASE_OUT, patrol_x_time)
-#		tween.interpolate_property(self, "global_position:x", eXtents.x,
-#		og_pos.x, patrol_x_time, Tween.TRANS_CUBIC, Tween.EASE_OUT, 2*patrol_x_time)
-#	tween.repeat = true
-#	tween.start()
-
-
+onready var actual_pos = global_position  # godot wtf
 func _physics_process(delta):
-	if moving: global_position += velocity * delta
+	if moving: 
+		actual_pos += velocity * delta
+	if patrol_x != Vector2(): 
+		actual_pos.x = lerp(actual_pos.x, x_target, lerp_x)
+		if abs(global_position.x - x_bound.y) <= lee: x_target = x_bound.x 
+		if abs(global_position.x - x_bound.x) <= lee: x_target = x_bound.y
+	if patrol_y != Vector2():
+		actual_pos.y = lerp(actual_pos.y, y_target, lerp_y)
+		if abs(global_position.y - y_bound.y) <= lee: y_target = y_bound.x 
+		if abs(global_position.y - y_bound.x) <= lee: y_target = y_bound.y
+	global_position = actual_pos
--- misc/platSpawnerSP.gd
+++ misc/platSpawnerSP.gd
@@ -23,12 +23,15 @@ func _physics_process(delta):
 		for x in range(1,numPlats+1):
 			var pos = 1920/(numPlats+1) * x
 			var mid = (numPlats==3 && x==2)or(numPlats==4 && x==2)or(numPlats==4 && x==3)or(numPlats==1)
-			if numPlats == 1: scl = 6
-			if numPlats == 2: scl = 5
-			if numPlats == 3: scl = 4.5
-			if numPlats == 4: scl = 4
-			if mid: scl += 1
 			var plt = load(L[midRan]).instance() if mid else load(L[ran]).instance()
+			if numPlats == 1: 
+				scl = 5
+				plt.patrol_x = Vector2(-400,400)
+			if numPlats == 2: scl = 4.5
+			if numPlats == 3: scl = 4
+			if numPlats == 4: scl = 3.5
+#			if mid: scl += 1
+			plt.moving = true
 			plt.scale = Vector2(scl,scl)
 			plt.global_position = Vector2(pos, -40)
 			add_child(plt)
