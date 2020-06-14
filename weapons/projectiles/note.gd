a5fa8f1413496811c5215808a3d4b09a716a1fc7
henryliu <henryliuser@gmail.com>
2020-06-14 09:34:10 -0700

    trumpet shotgun online

--- weapons/projectiles/note.gd
+++ weapons/projectiles/note.gd
@@ -1,24 +1,32 @@
 extends Area2D
-export var damage = 5
+export var damage = 3
 onready var sprite = $sprite
 onready var timer = $Timer
+onready var tween = $Tween
 var velocity = Vector2()
 var rot_speed_deg = 0
 
 func _ready():
 	sprite.material = load("res://assets/Shaders/Outline.tres").duplicate()
 	sprite.material.set_shader_param("width", 0.5)
-	sprite.material.set_shader_param("outline_color", Color(randi()%16777215))
-	rot_speed_deg = randi()%401 - 200
+	var c = Color(randf(), randf(), randf())
+	sprite.material.set_shader_param("outline_color", c)
+	tween.interpolate_property(sprite, "modulate:a", sprite.modulate.a, 
+		0, timer.wait_time-0.1, Tween.TRANS_CUBIC, Tween.EASE_IN)
 	timer.start()
+	tween.start()
 
 func _physics_process(delta):
 	global_position += velocity*delta
 	sprite.global_rotation_degrees += rot_speed_deg*delta
+#	sprite.modulate.a = lerp(sprite.modulate.a, 0, 0.02)
 
 func _on_note_body_entered(body):
 	if body.has_method("getHurt"):
 		body.getHurt(damage, 10, Vector2(200, -200), global_position)
+		sprite.frame = 0
+		$hitbox.queue_free()
+		yield(get_tree().create_timer(0.2, false), "timeout")
 		queue_free()
 
 func _on_Timer_timeout():
--- weapons/trumpetShotgun.gd
+++ weapons/trumpetShotgun.gd
@@ -1,11 +1,16 @@
 extends "res://weapons/gun.gd"
 
 func shoot():
-	for x in range(6):
+	.shoot()
+	for x in range(8):
 		var n = load("res://weapons/projectiles/note.tscn").instance()
-		n.velocity = Vector2(player.currentDirection*(randi()%201+100), randi()%71-35)
+		n.velocity = Vector2(player.currentDirection*(randi()%251+450), randi()%201-75)
+		n.rot_speed_deg = randi()%601 - 300
 		get_tree().current_scene.add_child(n)
-		get_tree().current_scene.move_child(n, 1)
+#		get_tree().current_scene.move_child(n, 1)
 		n.global_position = $Muzzle.global_position
+	player.velocity = Vector2(player.currentDirection * -900, -500)
+	player.sprite.rotation_degrees += player.currentDirection * 30
+	rotation_degrees += 15
 		
 		
